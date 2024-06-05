33fa84c637fc9b0e9ac51b01f29daf29eec3e6b2
Stuti Dutta <stutidutta@aarkglobalinc.com>
2024-06-05 10:55:31 +0530

    Add project files.

---
+++ WpfApp8/MainWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+
+namespace WpfApp8
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnFire_Click(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Forms.FolderBrowserDialog dialog = new System.Windows.Forms.FolderBrowserDialog();
+            dialog.InitialDirectory = "C:\\Users\\dutta\\source";
+            dialog.ShowDialog();
+
+
+            /*MessageBox.Show();*/
+
+        }
+    }
+}
